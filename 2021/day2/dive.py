91e2943d8b40d5373b6c6e08461ad646b8a06297
Miikkael Peltokorpi <miikkael.peltokorpi@icloud.com>
2022-11-15 20:14:01 +0200

    refactoring and added some comments

--- 2021/day2/dive.py
+++ 2021/day2/dive.py
@@ -1,33 +1,36 @@
 #!/usr/bin/python3
 
 
-def calculate(x_position, y_position) -> int:
-    return x_position * y_position
+def calculate(x_position, depth) -> int:
+    """ Calculates final result """
+    return x_position * depth
     
    
-
 def main():
 
-    x_position = 0
-    depth = 0
-    filepath = 'day2.txt'
+    try:
+        file = open('day2.txt', "r")
 
-    file = open(filepath, "r")
+        x_position = 0
+        depth = 0
 
-    for line in file:
-        amount = int(line[-2])
-        command = line[:-3]
+        for line in file:
+            move_value = int(line[-2])
+            command = line[:-3]
 
-        if command == 'down':
-            depth += amount
-        if command == 'up':
-            depth -= amount
-        if command == 'forward':
-            x_position += amount
+            if command == 'down': # if command down. Dive towards the bottom of the sea
+                depth += move_value
+            if command == 'up': # if command up. Dive towards the surface
+                depth -= move_value
+            if command == 'forward': # if command forward. Move forward
+                x_position += move_value
 
-    
-    result = calculate(x_position, depth)
-    print(f"First answer is:{result}")
+        # Calculate final position.
+        result = calculate(x_position, depth)
+        print(f"Final answer is:{result}")
+        
+    except FileNotFoundError:
+        print('File not found')
   
 
 if __name__ == "__main__":
